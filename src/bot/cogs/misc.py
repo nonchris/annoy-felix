b72734756684c3d3f0af890778227985c04390a2
nonchris <git@chris-ge.de>
2021-09-30 11:15:56 +0200

    Add task to annoy felix

--- src/bot/cogs/misc.py
+++ src/bot/cogs/misc.py
@@ -1,4 +1,7 @@
-from discord.ext import commands
+from random import randint
+
+import discord
+from discord.ext import commands, tasks
 
 from ..log_setup import logger
 from ..utils import utils as ut
@@ -16,6 +19,7 @@ class Misc(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.annoy_felix.start()
 
     @commands.command(name='ping', help="Check if Bot available")
     async def ping(self, ctx):
@@ -31,6 +35,17 @@ class Misc(commands.Cog):
                 name='Bot is available',
                 value=f'`{round(self.bot.latency * 1000)}ms`')
         )
+
+    @tasks.loop(seconds=5.0)
+    async def annoy_felix(self):
+        send = randint(0, 1)
+        print(send)
+        if send:
+            guild: discord.Guild = self.bot.get_guild(884484070707437619)
+            channel: discord.TextChannel = guild.get_channel(893061956683038721)
+            print("Sending")
+            await channel.send("Hallo Felix")
+
 
 
 def setup(bot):
